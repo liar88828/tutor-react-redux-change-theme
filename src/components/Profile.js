71fea76a7e0dcbf189f00eed8578d00626cc2aeb
brian <liar88828@gmail.com>
2022-11-14 22:35:14 +0700

    polosss

---
+++ src/components/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Profile = () => {
+  return (
+    <div>
+      <h1>Profile</h1>
+      <p>Name : </p>
+      <p>Age : </p>
+      <p>Email : </p>
+    </div>
+  );
+};
